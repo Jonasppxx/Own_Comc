cfcc0fdb91e92ad9ae7179e15c910fd0628f4070
Jonasppxx <Jonas.boos@bwdbern.ch>
2024-08-26 19:33:06 +0200

    Update number_worth.py

--- today/number_worth.py
+++ today/number_worth.py
@@ -1,4 +1,4 @@
 price_cart = 3.0 # Setze den Mindestpreis für die Preisprüfung
 fertigen_waitingtime = 2 # Setze die Wartezeit zwischen dem fertigen in Sekunden
-remove_dates = False # Bei True werden nur die letzten zwei Tage behalten
+remove_dates = True # Bei True werden nur die letzten zwei Tage behalten
 scrape_comc = True # Setze auf False, wenn du nicht scrapen willst
