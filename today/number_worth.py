ac696a95df9810be1150e1fb1a7f0499fae7a6cb
Jonasppxx <Jonas.boos@bwdbern.ch>
2024-09-12 20:18:18 +0200

    Automatischer Commit am 2024-09-12 20:18:18

--- today/number_worth.py
+++ today/number_worth.py
@@ -1,4 +1,4 @@
-price_cart = 1 # Setze den Mindestpreis für die Preisprüfung
+price_cart = 0.3 # Setze den Mindestpreis für die Preisprüfung
 fertigen_waitingtime = 2 # Setze die Wartezeit zwischen dem fertigen in Sekunden
 remove_dates = True # Bei True werden nur die letzten zwei Tage behalten
-scrape_comc = True # Setze auf False, wenn du nicht scrapen willst
+scrape_comc = False # Setze auf False, wenn du nicht scrapen willst
